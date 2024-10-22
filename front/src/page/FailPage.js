10d4592b46ea5eb5c002d8f83dea0869e31f900e
DESKTOP-CL839OA\dlxksdnr <pp990417@naver.com>
2024-10-22 12:03:56 +0900

    add front project

--- front/src/page/FailPage.js
+++ front/src/page/FailPage.js
@@ -7,7 +7,7 @@ function FailPage() {
     return (
         <div>
             <h1>결제가 실패했습니다</h1>
-            <p>결제 중 문제가 발생했습니다. 다시 시도해 주세요.</p>
+            <p>결제 중 문제가 발생했습니다.</p>
             <button onClick={() => navigate('/')}>홈으로 돌아가기</button>
         </div>
     );
