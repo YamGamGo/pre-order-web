d739f614a1c6604fd92e15af9e907f7046a51bfa
DESKTOP-CL839OA\dlxksdnr <pp990417@naver.com>
2024-10-22 11:53:56 +0900

    no message

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import Homepage from './page/Homepage';
 import Information from "./page/Information";
 import PurchasePage from "./page/PurchasePage"; // 구매 페이지 임포트
 import SuccessPage from "./page/SuccessPage"; // 성공 페이지 임포트
+import FailPage from './page/FailPage';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route path="/information" element={<Information />} />
                 <Route path="/reservation/:id" element={<PurchasePage/>} /> {/* 구매 페이지 경로 */}
                 <Route path="/success" element={<SuccessPage />} /> {/* 성공 페이지 경로 */}
+                <Route path="/fail" element={<FailPage />} />
             </Routes>
         </Router>
     );
